ab9fbff81d956a96ae79d75491adba560732d33b
Cameron Sachse <cam.sachse1@gmail.com>
2021-11-29 14:35:28 -0500

    home page

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import {
 import { setContext } from '@apollo/client/link/context';
 
 import Home from './pages/Home';
+import Products from './pages/Products';
 import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
@@ -46,6 +47,7 @@ function App() {
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
+              <Route exact path="/Products" component={Products} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/success" component={Success} />
